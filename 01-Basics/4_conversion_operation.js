05426565205fdcdaa931a7a5f3863faba141be3f
Deepesh297 <deepesh.katudia15769@sakec.ac.in>
2024-12-19 17:03:03 -0500

    conversions

---
+++ 01-Basics/4_conversion_operation.js
@@ -0,0 +1,40 @@
+let score = "33abc"  
+
+// let score = null = 0 
+
+// let score = undefined = NaN
+
+
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* 
+    33abc = NaN
+    abc = NaN
+    null = 0
+    undefined = NaN
+    boolean = true - 1 and false - 0
+*/
+
+let isLoggedin = 1 
+let booleanisLoggedin = Boolean(isLoggedin)
+
+console.log(isLoggedin);
+
+console.log(booleanisLoggedin);
+
+// "" = false
+// "deepesh" = true
+
+let somenumber = 55
+
+let stringnumber = String(somenumber)
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
