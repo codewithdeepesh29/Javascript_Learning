25f4761c4f5fa11d092d720cb05b507fbc06a639
Deepesh297 <deepesh.katudia15769@sakec.ac.in>
2024-12-19 16:29:40 -0500

    datatypes

---
+++ 01-Basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// alert(3+3) this is uded in browser coding with html and not in normal code 
+
+let name= "Deepesh" // String
+let age = 18 // Number 
+let isLoggedin = false  // Boolean
+let state = null
+
+// number = 2 to power 53
+
+// Bigint - used when number is very long
+
+// null - Standalone value
+
+// undefined - value is not defined , declare var and not given value
+
+// Symbol - defines for uniquness for making components in react 
+
+/* 
+    Non - primitive data types
+
+    1. Object
+
+*/
+
+console.log(typeof state);  // Object
+console.log(typeof undefined);  // undefined
---
+++ 01-Basics/variables.js
@@ -0,0 +1,21 @@
+const accountId = 45
+let accountEmail = "deepesh@pace.edu"
+var accountPassword = "Deepesh@"
+accountCity = "Mumbai"
+let accountState
+
+// accountId = 1 // Cannot be changed as it has 'const' keyword
+
+accountEmail = "miteshkatudia@gmai.com"
+
+accountPassword = "Mitesh"
+
+accountCity = "mum"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// Don not use var because of block scope and functional scope 
+
+
+
+// console.log(accountId);
