e4b4d1a8f6c9991e941c8be0606f746384ab72fc
Deepesh297 <deepesh.katudia15769@sakec.ac.in>
2024-12-22 20:19:54 -0500

    Stack & Heap

--- 01-Basics/5_comparison.js
+++ 01-Basics/5_comparison.js
@@ -13,4 +13,5 @@ console.log(undefined>0);
 console.log(undefined==0);
 console.log(undefined>=0);
 
+console.log("2" === 2);
 
---
+++ 01-Basics/memory.js
@@ -0,0 +1,27 @@
+// Stack (Primitive) and Heap (Non-Primitive)
+
+let youtubename = "Deepesh09"
+
+let anothername = youtubename
+
+anothername = "Katudia"
+
+console.log(anothername);
+console.log(youtubename);
+
+
+let user1 = {
+    email : "deepeshkatudia@gmail.com",
+    upi : "user@ybl",
+}
+
+let user2 = user1
+
+user2.email = "katudia@gmail.com" // acces object elements like this 
+
+console.log(user1.email);
+console.log(user2.email);
+
+
+
+
