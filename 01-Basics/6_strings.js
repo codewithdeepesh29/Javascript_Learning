1776dc73d9a8fedb387260c13e0305cda81e093c
Deepesh297 <deepesh.katudia15769@sakec.ac.in>
2024-12-23 20:34:11 -0500

    Strings

---
+++ 01-Basics/6_strings.js
@@ -0,0 +1,14 @@
+const name = "Deepesh"
+const repocount = 10
+
+console.log(name + repocount + "value"); // concatenate string do not use this outdated
+
+
+// modern way to use the string and ${} is use to call any variables present in the code 
+// String Inter Polation 
+console.log(`Hello my name is ${name} and my repository count is ${repocount}`);
+
+// use string using object 
+
+const gamename = new String("Deepesh")
+
